6fa008699861d33aedb801b3503219ad9f666a4a
Krishna Kumar <krishnakumar.1905059@srec.ac.in>
2023-06-13 17:58:03 +0530

    initial commit

---
+++ src/main/java/com/developer/demo/ShoppingMallManagementSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.developer.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingMallManagementSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingMallManagementSoftwareApplication.class, args);
+	}
+
+}
